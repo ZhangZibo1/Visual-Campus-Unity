9272685f216872f50b09e7e28b0b3a271a582584
ZhangZibo1 <3277718505@qq.com>
2023-08-24 20:38:53 +0800

    2023.8.24

---
+++ Assets/2023_7_19_Test/SpecialProcessScripts/Fading.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Fading : MonoBehaviour
+{
+    public float fadingPercentage = 0.3f;
+
+    Color orgiCImage;
+    Color origColorT;
+    Image im;
+    TextMeshProUGUI te;
+    // Start is called before the first frame update
+    void Start()
+    {
+        orgiCImage = GetComponent<Image>().color;
+        origColorT = transform.GetChild(0).GetComponent<TextMeshProUGUI>().color;
+        im = GetComponent<Image>();
+        te = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Description.eyeOnPanel)
+        {
+            im.color = orgiCImage * fadingPercentage;
+            te.color = origColorT * fadingPercentage;
+        }
+        else 
+        {
+            im.color = orgiCImage;
+            te.color = origColorT;
+        }
+        
+    }
+}
